60f4a577132f4aa765eaa74a2278aec5d44089a7
Max Underwood <maxmunderwood@yahoo.co.uk>
2015-12-05 19:49:07 +0000

    V1.0 - working well

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,4 +2,17 @@ count = 0
 
 while count < 100:
 	count  = count + 1
-	print(count)
+	current = count
+	if (current %3 == 0) and (current %5 == 0):
+		print("FizzBuzz")
+		continue
+
+	if current %3 == 0:
+		print("Fizz")
+		continue
+
+	if current %5 == 0:
+		print("Buzz")
+		continue
+
+	print(current)
